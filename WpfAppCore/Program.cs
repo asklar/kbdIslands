13a05fa6880907c16f52e87b8ac67dba69ccc7a4
Alexander Sklar <asklar@microsoft.com>
2021-04-08 02:28:26 -0700

    first commit

---
+++ WpfAppCore/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WpfAppCore
+{
+    public class Program
+    {
+        [System.STAThreadAttribute()]
+        public static void Main()
+        {
+            using (new UwpApp1.App())
+            {
+                var app = new WpfAppCore.App();
+                app.InitializeComponent();
+                app.Run();
+            }
+        }
+    }
+}
